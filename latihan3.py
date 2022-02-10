8f3055f287e6fff311d074ea596f119976829958
B1ass <99310872+B1ass@users.noreply.github.com>
2022-02-10 09:15:37 +0700

    Add files via upload

---
+++ latihan3.py
@@ -0,0 +1,9 @@
+inputA = input("masukan angka A : ")
+angkaA = int(inputA)
+
+inputB = input("masukan angka  B : ")
+angkaB = int(inputB)
+
+hasil_penjumlahan = angkaA + angkaB
+
+print(angkaA, " + ", angkaB, " = ", hasil_penjumlahan)
