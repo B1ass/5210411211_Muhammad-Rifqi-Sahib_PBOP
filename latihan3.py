069f0b6019013f7c67faa9aeaf2c23951c23bf9e
B1ass <99310872+B1ass@users.noreply.github.com>
2022-02-10 09:20:03 +0700

    Delete latihan3.py

--- latihan3.py
+++
@@ -1,9 +0,0 @@
-inputA = input("masukan angka A : ")
-angkaA = int(inputA)
-
-inputB = input("masukan angka  B : ")
-angkaB = int(inputB)
-
-hasil_penjumlahan = angkaA + angkaB
-
-print(angkaA, " + ", angkaB, " = ", hasil_penjumlahan)
